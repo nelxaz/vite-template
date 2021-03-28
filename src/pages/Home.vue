7bc24309abc1aa084c93433d7ab31697699b03d1
Nelson Zambrano <nelsonx.zambrano@gmail.com>
2021-03-28 19:46:33 -0400

    code: Use store for test

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,4 +1,10 @@
 <template>
   <div>Home route</div>
+  <div>{{ store.counter }}</div>
+  <button @click="store.increment">increment</button>
 </template>
-<script setup></script>
+<script setup>
+  import { useStore } from '@/store/store.js'
+
+  const store = useStore()
+</script>
