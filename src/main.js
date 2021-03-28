8b91b22a636926f0acf1fbe0bc8f78bf4b7c9c1c
Nelson Zambrano <nelsonx.zambrano@gmail.com>
2021-03-28 19:44:42 -0400

    code: Add pinia for stage management

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { createPinia } from 'pinia'
 import routes from 'virtual:generated-pages'
 import App from './App.vue'
 
@@ -10,4 +11,5 @@ const router = createRouter({
 const app = createApp(App)
 
 app.use(router)
+app.use(createPinia())
 app.mount('#app')
