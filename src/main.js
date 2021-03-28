7a33fbbb266e5363bcf0deccdb4c9584b3032809
Nelson Zambrano <nelsonx.zambrano@gmail.com>
2021-03-28 19:12:11 -0400

    code: Add routing configuration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from 'virtual:generated-pages'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
